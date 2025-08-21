cf4331f0e62a0112920d34294f620adb0bac5318
Bhupendra Kumar <149204876+bhupendragangwar@users.noreply.github.com>
2025-08-21 05:59:03 +0000

    Wrap inside th CartProvider

--- 03_ContextAPI/src/App.jsx
+++ 03_ContextAPI/src/App.jsx
@@ -2,21 +2,29 @@ import './App.css'
 
 // Import Pages
 
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contect'
-import Footer from './Common/Footer'
+// import Home from './pages/Home'
+// import About from './pages/About'
+// import Contact from './pages/Contect'
+// import Footer from './Common/Footer'
+// import { UserProvider } from './contextProvider/MainContext'
 
-import { UserProvider } from './contextProvider/MainContext'
+import { CartProvider } from './cartLogic/Cartcontext'
+import Product from './cartLogic/Product'
+import CartStatus from './cartLogic/CartStatus'
 
 function App() {
 
   return (
-    <UserProvider>
-    <h1>Context API in React</h1>
-    <Home />
-    <Footer />
-    </UserProvider>
+    // <UserProvider>
+    // <h1>Context API in React</h1>
+    // <Home />
+    // <Footer />
+    // </UserProvider>
+    <CartProvider>
+      <h1>🛒 Shopping Cart with Context API</h1>
+      <Product />
+      <CartStatus />
+    </CartProvider>
   )
 }
 
