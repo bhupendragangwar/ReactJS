1b0f3e9649d44e08016632396bc96f2a3090a7ae
Bhupendra Kumar <149204876+bhupendragangwar@users.noreply.github.com>
2025-08-25 06:23:08 +0000

    fetch data from fetch api

--- 04_ApiHandling/src/App.jsx
+++ 04_ApiHandling/src/App.jsx
@@ -1,58 +1,39 @@
-// Use fetch() to get data from a 
-// public API (e.g., JSONPlaceholder) and display it in a list.
+import Fetch from './fetchApi/Fetch'
 
-import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [post, setPost] = useState([])
-  const [loading, setLading] = useState(true)
-  const [error, setError] = useState(null)
+  // const [post, setPost] = useState([])
+  // const [loading, setLading] = useState(true)
+  // const [error, setError] = useState(null)
 
-  const user = [
-    {id: 1, Name: "Rohit", add: 10},
-    {id: 2, Name: "Deepanshu", add: 11},
-    {id: 3, Name: "Ashish", add: 12},
-    {id: 4, Name: "Shivam", add: 13},
-    {id: 5, Name: "Vivek", add: 14},
-    {id: 6, Name: "Darmesh", add: 15},
-  ]
+  // useEffect(()=>{
+  //   const apiUrl = "https://jsonplaceholder.typicode.com/users"
+  //   fetch(apiUrl)
+  //   .then((res)=>{
+  //     if(!res.ok){
+  //       throw new Error("Network response was not ok")
+  //     }
+  //     return res.json()
+  //   })
+  //   .then((data)=>{
+  //     // console.log(data)
+  //     setPost(data);
+  //     setLading(false)
+  //   })
+  //   .catch((err) => {
+  //     setError(err);
+  //     setLading(false)
+  //   })
 
-  useEffect(()=>{
-    const apiUrl = "https://jsonplaceholder.typicode.com/users"
-    fetch(apiUrl)
-    .then((res)=>{
-      if(!res.ok){
-        throw new Error("Network response was not ok")
-      }
-      return res.json()
-    })
-    .then((data)=>{
-      // console.log(data)
-      setPost(data);
-      setLading(false)
-    })
-    .catch((err) => {
-      setError(err);
-      setLading(false)
-    })
-
-  }, [])
-  if(loading) return <p>Loading post...</p>
-  if(error) return <p>Error; {error}</p>
+  // }, [])
+  // if(loading) return <p>Loading post...</p>
+  // if(error) return <p>Error; {error}</p>
 
   return (
     <>
     <h2 className='hading'>API Handling</h2>
-    <ul>
-      {post.map((post)=>(
-        <li key={post.id}>
- <h3>{post.id}</h3>
-          <strong>{post.name}</strong>
-          <p>{post.username}</p>
-        </li>
-      ))}
-    </ul>
+    <Fetch />
     </>
   )
 }
---
+++ 04_ApiHandling/src/fetchApi/Fetch.jsx
@@ -0,0 +1,49 @@
+// Use fetch() to get data from a 
+// public API (e.g., JSONPlaceholder) and display it in a list.
+
+import { useState, useEffect } from 'react'
+
+const Fetch = () => {
+    const [post, setPost] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const apiUrl = "https://jsonplaceholder.typicode.com/users"
+        fetch(apiUrl)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Network res was not ok')
+                }
+                return res.json()
+            })
+            .then((data)=>{
+                console.log(data);
+                setPost(data);
+                setLoading(false)
+            })
+            .catch((err)=>{
+                setError(err)
+                setLoading(false)
+            })
+    }, [])
+    if (loading) return <p>Loading Post...</p>
+    if (error) return <p>Error: {error.message}</p>
+
+    return (
+        <>
+            <h2>Data Fetch from Fetch API</h2>
+            <uL>
+                {post.map((data)=>(
+                    <li key={data.id}>
+                        <h2>{data.id}</h2>
+                        <h2>{data.name}</h2>
+                    </li>
+                ))}
+            </uL>
+        </>
+    )
+
+}
+
+export default Fetch;
